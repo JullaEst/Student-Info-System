aba2445ad47ae60eb6894a20d49cdcc8fe442d07
JullaEst <42329789+JullaEst@users.noreply.github.com>
2019-10-29 12:26:22 -0700

    Create Roster.h

---
+++ Roster.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include "Student.h"
+#include "networkStudent.h"
+#include "securityStudent.h"
+#include "softwareStudent.h"
+
+class Roster {
+
+protected:
+	int lastIndex;
+	int capacity;
+	Student** classRosterArray;
+
+public:
+	Roster();
+	Roster(int capacity);
+	Student* getStudentAt(int index);
+
+	void parseThenAdd(string row);
+	void add(string studentId, string firstName, string lastName, string emailAddress, string age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeType degree);
+	void remove(string studentId);
+	void printAll();
+	void printAverageCourseDays(string studentId);
+	void printInvalidEmails();
+	void printByDegreeType(int degree);
+
+	~Roster();
+
+};
